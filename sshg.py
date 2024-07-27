7a1dd9f0c09aadc65c718df349f5cfc21a73a882
lisniuse <17560235@qq.com>
2024-07-27 19:38:02 +0800

    feat: 更新脚本功能

--- sshg.py
+++ sshg.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import pyperclip
 
 # 确定 SSH 密钥文件的路径
 ssh_dir = os.path.expanduser('~/.ssh')
@@ -12,20 +13,23 @@ def generate_ssh_key():
     subprocess.run(['ssh-keygen', '-t', 'rsa', '-b', '4096', '-f', private_key_path, '-N', ''], check=True)
     print(f"SSH 密钥对已生成：{private_key_path} 和 {public_key_path}")
 
-def print_public_key():
-    """打印公钥的内容"""
+def print_and_copy_public_key():
+    """打印公钥的内容并将其复制到剪贴板"""
     with open(public_key_path, 'r') as pub_key_file:
         public_key = pub_key_file.read()
-    print("现有的 SSH 公钥是：")
+    print("SSH 公钥是：")
     print(public_key)
+    pyperclip.copy(public_key)
+    print("公钥内容已复制到剪贴板。")
 
 def main():
     # 检查 SSH 密钥文件是否存在
     if os.path.exists(private_key_path) and os.path.exists(public_key_path):
-        print_public_key()
+        print_and_copy_public_key()
     else:
         # 如果不存在，则生成新的 SSH 密钥对
         generate_ssh_key()
+        print_and_copy_public_key()
 
 if __name__ == "__main__":
     main()
